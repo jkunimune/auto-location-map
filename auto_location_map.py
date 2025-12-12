144f49316f6ba734748142aef833c3d96bde037b
jkunimune <justinkunimune@gmail.com>
2025-12-11 22:17:47 -0500

    make coastlines work correctly
    whew, that was way more complicated than I feel like it should have been.  I added a function to stitch together multipolygons into regular polygons and another function on top of that to add the closure to the coastline.  I had to break the coastlines and lakes into two separate layers because it turns out some water features are multipolygons whose edges are not garanteed to have any particular direction which means they don't work in the coastline closure algorithm.

--- auto_location_map.py
+++ auto_location_map.py
@@ -1,5 +1,6 @@
 import argparse
-from math import sqrt, cos, radians
+from itertools import chain
+from math import pi, sqrt, cos, radians, atan2
 from os import makedirs, path
 import re
 from time import time, sleep
@@ -120,8 +121,11 @@ def choose_queries(major_streets, minor_streets, railroads, parks, x_scale):
 
 	# put together the tags that define the relevant data
 	shape_types = {
-		"water": [
-			("nwr", "natural", r"^(water|coastline)$"),
+		"sea": [
+			("nwr", "natural", r"^coastline$"),
+		],
+		"lake": [
+			("nwr", "natural", r"^water$"),
 		],
 		"highway": [
 			("way", "highway", r"^(motorway|trunk)$"),
@@ -186,7 +190,8 @@ def write_SVG(new_filename, bbox, x_scale, y_scale, shape_types, data):
 			f'\t<desc>A location map of the region with latitudes between {bbox.south} and {bbox.north}, and longitudes between {bbox.west} and {bbox.east}.  Equirectangular projection.  The data for this map is made available by the OpenStreetMap contributors, under the Open Database License: http://opendatacommons.org/licenses/odbl/1.0/</desc>\n'
 			f'\t<style>\n'
 			f'\t\t.background {{ fill: #ffffff; stroke: none }}\n'
-			f'\t\t.water {{ fill: #b1deff; fill-rule: evenodd; stroke: none }}\n'
+			f'\t\t.sea {{ fill: #b1deff; fill-rule: evenodd; stroke: none }}\n'
+			f'\t\t.lake {{ fill: #b1deff; fill-rule: evenodd; stroke: none }}\n'
 			f'\t\t.green {{ fill: #d5f5da; fill-rule: evenodd; stroke: none }}\n'
 			f'\t\t.sand {{ fill: #fde8c6; fill-rule: evenodd; stroke: none }}\n'
 			f'\t\t.airport {{ fill: #f0e9ed; fill-rule: evenodd; stroke: none }}\n'
@@ -194,10 +199,11 @@ def write_SVG(new_filename, bbox, x_scale, y_scale, shape_types, data):
 			f'\t\t.highway {{ fill: none; stroke: #b79bad; stroke-width: 1.06; stroke-linejoin: round; stroke-linecap: round }}\n'
 			f'\t\t.railroad {{ fill: none; stroke: #93898f; stroke-width: 0.53; stroke-linejoin: round; stroke-linecap: round }}\n'
 			f'\t</style>\n'
+			f'\t<rect class="background" x="0" y="0" width="100%" height="100%" />\n'
 		)
 
 		# for each type of data, in order
-		for shape_type in ["water", "green", "sand", "airport", "minor_street", "major_street", "highway", "railroad"]:
+		for shape_type in ["sea", "green", "sand", "airport", "lake", "minor_street", "major_street", "highway", "railroad"]:
 			if shape_type not in shape_types:
 				continue
 
@@ -212,18 +218,29 @@ def write_SVG(new_filename, bbox, x_scale, y_scale, shape_types, data):
 			if len(shapes) == 0:
 				continue
 
-			# convert it to SVG paths
-			file.write(f'\t<g class="{shape_type}">\n')
+			# pull out the geometry and post-process it into nice polygons
+			paths = []
 			for shape in shapes:
 				if shape["type"] == "way":
-					points = [shape["geometry"]]
+					paths.append([shape["geometry"]])
 				elif shape["type"] == "relation":
-					points = [member["geometry"] for member in shape["members"]]
+					path = []
+					for member in shape["members"]:
+						path.append(member["geometry"])
+					paths.append(path)
 				else:
 					raise TypeError(shape["type"])
+			if shape_type == "sea":  # for coastlines, don't forget to stitch them together
+				paths = consolidate_all_polygons(paths, bbox)
+			else:  # for other layers, you still do some stitching but it's not as complicated
+				paths = consolidate_multipolygons(paths)
+
+			# convert it to SVG paths
+			file.write(f'\t<g class="{shape_type}">\n')
+			for path in paths:
 				path_string = ""
-				for section in points:
-					for i, point in enumerate(section):
+				for segment in path:
+					for i, point in enumerate(segment):
 						command = "M" if i == 0 else "L"
 						x = x_scale*(point["lon"] - bbox.west)
 						y = y_scale*(point["lat"] - bbox.north)
@@ -232,6 +249,82 @@ def write_SVG(new_filename, bbox, x_scale, y_scale, shape_types, data):
 			file.write(f'\t</g>\n')
 		file.write("</svg>\n")
 	print(f"Saved the map to `maps/{new_filename}.svg`!")
+
+
+def consolidate_multipolygons(paths):
+	finished_paths = []
+	for path in paths:
+		finished_segments = []
+		pending_segments = path[:]
+		while len(pending_segments) > 0:
+			# examine the last segment
+			last_segment = pending_segments.pop()
+			# and search for another segment that starts or ends with its endpoint
+			next_segment = None
+			for i in range(len(pending_segments)):
+				if pending_segments[i][0] == last_segment[-1]:
+					next_segment = pending_segments.pop(i)
+					break
+				elif pending_segments[i][-1] == last_segment[-1]:
+					next_segment = pending_segments.pop(i)[::-1]
+					break
+			# if no one starts with its endpoint, or it starts with its own endpoint, we're done here
+			if next_segment is None or next_segment == last_segment:
+				finished_segments.append(last_segment)
+			# if you found another one that starts with its endpoint, stick them together and repeat
+			else:
+				last_segment += next_segment
+				pending_segments.append(last_segment)
+		# when you run out of segments, you're done
+		finished_paths.append(finished_segments)
+	return finished_paths
+
+
+def consolidate_all_polygons(paths, bbox):
+	# define this little utility function real quick
+	def angular_distance(point_A, point_B):
+		θ_A = atan2(
+			point_A["lat"] - (bbox.north + bbox.south)/2,
+			point_A["lon"] - (bbox.east + bbox.west)/2)
+		θ_B = atan2(
+			point_B["lat"] - (bbox.north + bbox.south)/2,
+			point_B["lon"] - (bbox.east + bbox.west)/2)
+		if θ_A >= θ_B:
+			return θ_A - θ_B
+		else:
+			return θ_A - θ_B + 2*pi
+
+	# first, run the regular consolidation algorithm on all the paths together
+	megapath = list(chain(*paths))
+	megapath = consolidate_multipolygons([megapath])[0]
+
+	# first, add the corners to the set of segments to stitch together
+	open_segments = [
+		[{"lat": bbox.north, "lon": bbox.east}],
+		[{"lat": bbox.north, "lon": bbox.west}],
+		[{"lat": bbox.south, "lon": bbox.east}],
+		[{"lat": bbox.south, "lon": bbox.west}],
+	] + megapath
+	# then, pull off arbitrary open segments and try to complete them
+	closed_segments = []
+	while len(open_segments) > 0:
+		new_closed_segment = open_segments[-1]
+		while True:
+			next_segment = min(
+				open_segments,
+				key=lambda segment: angular_distance(new_closed_segment[-1], segment[0])
+			)
+			if next_segment == new_closed_segment:
+				open_segments.remove(new_closed_segment)
+				if len(new_closed_segment) > 1:
+					closed_segments.append(new_closed_segment)
+				break
+			else:
+				open_segments.remove(next_segment)
+				new_closed_segment += next_segment
+
+	return [closed_segments]
+
 
 
 class BoundingBox:
